4b65759c0975423a31b81ae0c9aa93dafa0bc6a2
Margen Peterson <peterson.margen@gmail.com>
2024-06-12 00:23:24 +0300

    nextjs init, basic routing

---
+++ src/app/builder/page.tsx
@@ -0,0 +1,10 @@
+
+export default function BuilderPage() {
+  return (
+    <main className="flex flex-1 flex-col p-4 md:p-6">
+      <div className="flex items-center mb-8">
+        <h1 className="font-semibold text-lg md:text-2xl">BUILDER</h1>
+      </div>
+    </main>
+  );
+}
