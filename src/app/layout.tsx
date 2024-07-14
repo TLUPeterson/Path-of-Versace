0d81533e64fbda65279057a2175683f378fc31cb
Margen Peterson <peterson.margen@gmail.com>
2024-07-14 14:48:59 +0300

    'ta'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,6 @@ export default function RootLayout({
         <div className="grid min-h-screen w-full">
           <div className="flex flex-col w-full">
             <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40 justify-between lg:justify-end">
-              <Link className="flex items-center gap-2 font-semibold lg:hidden" href="/">
-                <Logo />
-                <span className="">ACME</span>
-              </Link>
 
               <NavItem href="/">
                 <UsersIcon className="h-4 w-4" />
